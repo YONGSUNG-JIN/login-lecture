d3ea001c795c40d8d781c1e7233b373c8a7dee43
Leo Jin <jinys90@gmail.com>
2022-06-23 23:16:09 +0900

    폴더구조 체적화

---
+++ app/app.js
@@ -0,0 +1,20 @@
+"use strict";
+
+//모듈
+const express = require("express");
+const app = express();
+
+const PORT = 3000;
+
+
+//라우팅
+const home = require("./src/routes/home");
+
+//앱 세팅
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+
+app.use("/", home); // 미들웨어 등록 메써드
+
+module.exports = app;
+
