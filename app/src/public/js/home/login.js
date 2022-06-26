f05b2d3ac63c766f6ea68f77f927723a4fd6001f
Leo Jin <jinys90@gmail.com>
2022-06-26 12:58:35 +0900

    백앤드에서 로그 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -19,8 +19,10 @@ function login(){
         method : "POST",
         headers:{
             "Content-Type" : "application/json"
-        },
+        }, 
         body : JSON.stringify(req),
     })
+    .then((res)=>res.json())
+    .then((res)=>{console.log(res)});
 
 }
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,14 +1,43 @@
 "use strict";
 
-const home = (req,res)=>{
-    res.render("home/index");
+const output = {
+    home: (req, res) => {
+        res.render("home/index");
+    },
+    login: (req, res) => {
+        res.render("home/login");
+    }
 }
 
-const login = (req,res)=>{
-    res.render("home/login");
+const users = {
+    id: ["jinys90", "jinyh95", "ys.jin@fastfive.co.kr"],
+    psword: ["1234", "5678", "123456"],
+};
+
+const process = {
+    login: (req, res) => {
+        const id = req.body.id,
+            psword = req.body.psword;
+
+        console.log(id, psword);
+
+        if (users.id.includes(id)) {
+            const idx = users.id.indexOf(id);
+            if (users.psword[idx] === psword) {
+                return res.json({
+                    success: true,
+                });
+            }
+        }
+        return res.json({
+            success: false,
+            msg: "로그인에 실패"
+        });
+    }
 }
+
 
 module.exports = {
-    home : home,
-    login : login
+    output: output,
+    process,
 };
